9b20a6ce6a03e3047f0b688c4076ec42697e7a44
Ben Turner <bbgrabbag@gmail.com>
2018-01-06 15:06:26 -0700

    basic working blockchain prototype

---
+++ sandbox/index.js
@@ -0,0 +1,54 @@
+const uuid = require("uuid");
+const ursa = require("ursa");
+const bcrypt = require("bcrypt");
+const salt = bcrypt.genSaltSync(10);
+
+class Blockchain {
+    constructor(tx) {
+        this.users = ["mint", ...Array.from(Object.keys(tx))];
+        this.chain = new Block("mint", tx);
+    }
+    addBlock(block) {
+        this.chain = block;
+    }
+    validateChain() {
+        let currBlock;
+        do {
+            currBlock = this.chain;
+        } while (!currBlock.prevHash === "0") {
+            let data = JSON.stringify(currBlock.prevBlock);
+            let hash = currBlock.prevHash;
+            if (!bcrypt.compareSync(data, hash)) {
+                return false;
+            }
+            currBlock = currBlock.prevBlock;
+        }
+        return true;
+    }
+}
+
+class Block {
+    constructor(sender, tx, prevBlock = null) {
+        //id
+        this.id = uuid();
+        //prev block
+        this.prevBlock = prevBlock;
+        //hash of prev block
+        this.prevHash = !prevBlock ? "0" : bcrypt.hashSync(JSON.stringify(prevBlock), 10);
+        this.tx = tx;
+        this.sender = sender;
+    }
+}
+
+let user1 = uuid();
+let user2 = uuid();
+
+let benCoin = new Blockchain({
+    [user1]: 3,
+    [user2]: 5
+});
+benCoin.addBlock(new Block(user1, {
+    [user2]: 3
+}, benCoin.chain));
+
+console.log((benCoin.validateChain()));
